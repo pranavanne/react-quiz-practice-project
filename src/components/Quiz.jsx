35719c1e3f75f56dc9f5c8184450c4199922c876
Pranav Anne <pranav.anne21@gmail.com>
2025-02-03 17:29:01 -0500

    Option now shuffle and added end screen

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,13 +1,27 @@
 import { useState } from "react";
 
 import QUESTIONS from "../questions.js";
-import questions from "../questions.js";
+import quizCompleteImage from "../assets/quiz-complete.png";
+
 
 export default function Quiz() {
 
     const [userAnswers, setUserAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
+
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+    if(quizIsComplete) {
+        return (
+            <div id="summary">
+                <img src={quizCompleteImage} alt="Trophy icon" />
+                <h2>Quiz Completed!</h2>
+            </div>
+        );
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort((a, b) => Math.random() - 0.5);
 
     function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prev) => {return [...prev, selectedAnswer]});
@@ -18,7 +32,7 @@ export default function Quiz() {
             <div id="question">
             <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
             <ul id="answers">
-                {QUESTIONS[activeQuestionIndex].answers.map((option) => <li key={option} className="answer"><button onClick={() => {handleSelectAnswer(option)}}>{option}</button></li>)}
+                {shuffledAnswers.map((option) => <li key={option} className="answer"><button onClick={() => {handleSelectAnswer(option)}}>{option}</button></li>)}
             </ul>
             </div>
         </div>
